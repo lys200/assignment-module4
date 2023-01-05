d2e3c91564e65ed658dc7b681bee9e819f74526a
lys200 <121077170+lys200@users.noreply.github.com>
2023-01-04 23:50:41 -0500

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function (widow) {
-  var helloSpeaker = {};
-  helloSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
-
-  };
-  var speakWord = "Hello";
-  window.helloSpeaker = helloSpeaker;
-})(window);
